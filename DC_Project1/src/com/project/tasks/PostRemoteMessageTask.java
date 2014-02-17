44aa1664286a9900309513ffca6967853758f63f
Nick C. <visitor15@gmail.com>
2014-02-17 16:16:00 -0500

    Working on networked data transfers.

--- DC_Project1/src/com/project/tasks/PostRemoteMessageTask.java
+++ DC_Project1/src/com/project/tasks/PostRemoteMessageTask.java
@@ -1,6 +1,9 @@
 package com.project.tasks;
 
-import java.io.DataOutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -47,26 +50,53 @@ public class PostRemoteMessageTask extends SimpleAbstractTask {
 				// m_SendingSocket = new Socket(connectedPeers.get(0).getIP(),
 				// connectedPeers.get(0).getMessagePort());
 
-				m_SendingSocket = new Socket("192.168.1.9", 9797);
-
+//				m_SendingSocket = new Socket("192.168.1.9", 9797);
+//
 				PostMessageTask task = new PostMessageTask();
 				task.setMessage(message);
+//				
+//				System.out.println("Wrote " + task.toBytes().length + " bytes");
+//
+//				DataOutputStream dataOutStream = new DataOutputStream(m_SendingSocket.getOutputStream());
+//				
+//				dataOutStream.writeUTF(getTaskId());
+//				dataOutStream.writeUTF(getStringData());
+//				dataOutStream.flush();
+//				dataOutStream.close();
+//				
+////				m_SendingSocket.getOutputStream().flush();
+////				m_SendingSocket.getOutputStream().close();
+//				m_SendingSocket.close();
 				
-				System.out.println("Wrote " + task.toBytes().length + " bytes");
+				
+//				 File file = new File("M:\\test.xml");
+				    // Get the size of the file
+//				    long length = file.length();
+//				    if (length > Integer.MAX_VALUE) {
+//				        System.out.println("File is too large.");
+//				    }
+				    byte[] bytes = task.toBytes();
+//				    FileInputStream fis = new FileInputStream(file);
+				    ByteArrayInputStream arrayInStream = new ByteArrayInputStream(bytes);
+				    BufferedInputStream bis = new BufferedInputStream(arrayInStream);
+				    BufferedOutputStream out = new BufferedOutputStream(m_SendingSocket.getOutputStream());
 
-				DataOutputStream dataOutStream = new DataOutputStream(m_SendingSocket.getOutputStream());
+				    int count;
+
+				    while ((count = bis.read(bytes)) > 0) {
+				        out.write(bytes, 0, count);
+				    }
+
+				    out.flush();
+				    out.close();
+//				    fis.close();
+				    bis.close();
+				    m_SendingSocket.close();
 				
-				dataOutStream.writeUTF(getTaskId());
-				dataOutStream.writeUTF(getStringData());
-				dataOutStream.flush();
-				dataOutStream.close();
-				
-//				m_SendingSocket.getOutputStream().flush();
-//				m_SendingSocket.getOutputStream().close();
-				m_SendingSocket.close();
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} while (!message.equalsIgnoreCase("/q"));
--- DC_Project1/src/com/project/tasks/ReceiveRemoteMessagesTask.java
+++ DC_Project1/src/com/project/tasks/ReceiveRemoteMessagesTask.java
@@ -1,20 +1,15 @@
 package com.project.tasks;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import com.project.framework.Task;
-import com.project.io.SynchedInOut;
 import com.project.server.router.Client;
 
 public class ReceiveRemoteMessagesTask extends SimpleAbstractTask {
@@ -40,52 +35,99 @@ public class ReceiveRemoteMessagesTask extends SimpleAbstractTask {
 
 	@Override
 	public void execute() {
-		
+
 		System.out.println("Listening for remote messages...");
 
 		String message = "NULL";
 
 		do {
 
+			// try {
+			// m_SendingSocket = new ServerSocket(LISTEN_PORT);
+			//
+			// m_RecievingSocket = m_SendingSocket.accept();
+			//
+			// InputStream sockInputStream = m_RecievingSocket.getInputStream();
+			//
+			//
+			// byte[] objLength = new byte[Long.SIZE / 8];
+			//
+			// sockInputStream.read(objLength, 0, (Long.SIZE / 8));
+			//
+			// ByteArrayInputStream is = new ByteArrayInputStream(objLength);
+			// ObjectInputStream in = new ObjectInputStream(is);
+			//
+			// long properLength = in.readLong();
+			//
+			//
+			// //
+			// // long byteArrayLength = in.readLong();
+			// // tmpStringData = in.readUTF();
+			// byte[] messageTaskBytes = new byte[(int) properLength];
+			//
+			// is.read(messageTaskBytes, objLength.length, (int)properLength);
+			//
+			
+			// PostMessageTask.fromNewBytes(messageTaskBytes);
+			//
+			// // PrintWriter out = new PrintWriter(
+			// // m_RecievingSocket.getOutputStream(), true);
+			// // BufferedReader in = new BufferedReader(
+			// // new InputStreamReader(
+			// // m_RecievingSocket.getInputStream()));
+			//
+			// TaskManager.DO_TASK(task);
+			//
+			//
+			// } catch (UnknownHostException e) {
+			// e.printStackTrace();
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// }
+
+			PostMessageTask task;
+			InputStream is = null;
+			ByteArrayOutputStream dataOutStream = null;
+			BufferedOutputStream bos = null;
+			int bufferSize = 0;
+
 			try {
-				m_SendingSocket = new ServerSocket(LISTEN_PORT);
-
 				m_RecievingSocket = m_SendingSocket.accept();
+			} catch (IOException ex) {
+				System.out.println("Can't accept client connection. ");
+			}
 
-				InputStream sockInputStream = m_RecievingSocket.getInputStream();
+			try {
+				is = m_RecievingSocket.getInputStream();
+
+				bufferSize = m_RecievingSocket.getReceiveBufferSize();
+				System.out.println("Buffer size: " + bufferSize);
+			} catch (IOException ex) {
+				System.out.println("Can't get socket input stream. ");
+			}
+
+			// fos = new FileOutputStream("M:\\test2.xml");
+			dataOutStream = new ByteArrayOutputStream();
+			bos = new BufferedOutputStream(dataOutStream);
+
+			byte[] bytes = new byte[bufferSize];
+
+			int count;
+
+			try {
+				while ((count = is.read(bytes)) > 0) {
+					bos.write(bytes, 0, count);
+				}
+
+				bos.flush();
+				bos.close();
+				is.close();
 				
-				
-				byte[] objLength = new byte[Long.SIZE / 8];
-				
-				sockInputStream.read(objLength, 0, (Long.SIZE / 8));
-				
-						ByteArrayInputStream is = new ByteArrayInputStream(objLength);
-				ObjectInputStream in = new ObjectInputStream(is);
-				
-				long properLength = in.readLong();
-				
-				
-//
-//				long byteArrayLength = in.readLong();
-//				tmpStringData = in.readUTF();
-				byte[] messageTaskBytes = new byte[(int) properLength];
-				
-				is.read(messageTaskBytes, objLength.length, (int)properLength);
-				
-				PostMessageTask task = (PostMessageTask) PostMessageTask.fromNewBytes(messageTaskBytes);
-				
-//				PrintWriter out = new PrintWriter(
-//						m_RecievingSocket.getOutputStream(), true);
-//				BufferedReader in = new BufferedReader(
-//						new InputStreamReader(
-//								m_RecievingSocket.getInputStream()));
-				
+				task = (PostMessageTask) PostMessageTask.fromNewBytes(dataOutStream.toByteArray());
+
 				TaskManager.DO_TASK(task);
-				
-				
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} while (!message.equalsIgnoreCase("/q"));
